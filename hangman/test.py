a619f59cf7bd4b09495eade1b7a5010e8a842a35
Gwyn <gwyn_g@live.com>
2022-08-14 13:00:04 +0100

    first commit; includes 1 required argument :'self' error

--- hangman/test.py
+++ hangman/test.py
@@ -1,2 +1,23 @@
-cube_dict = {i:i ** 3 for i in range (1,11)}
-print (cube_dict)
+
+from re import X
+
+
+list_letters = ["A","B"]
+
+
+class Hongman:
+
+    def check_letter ():
+        print ("Guess a new letter!")
+        letter = input ().upper() #will the guess be case sensitive? keep .upper in mind!! 
+        if len(letter) != 1:
+            print ("Please, enter just one character.")
+            return Hongman.check_letter()
+        if letter in list_letters:
+            print (f"{letter} has already been tried.")
+            return Hongman.check_letter()
+        return letter
+
+x = Hongman.check_letter()
+
+print (x + " is my guess!")
